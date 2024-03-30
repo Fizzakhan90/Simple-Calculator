66b1856b211ac3b6b9f2df78d254154246a7a073
Fizzakhan <165285280+Fizzakhan90@users.noreply.github.com>
2024-03-30 14:36:04 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"; 
+// printing a wellcome Message
+ console.log("\n\wellcome to \ 'codewithfizzakhan\' -cLI simple calculator\n ");
+
+// Asking Question from users through Inquirer
+
+const answer = await inquirer.prompt([{ message: "enter first Number:", type: "Number", name: "firstNumber" },
+{message: "enter second Number:", type: "Number", name: "secondNumber"},
+{message: "select one of the operators to perform operation", type: "list", name: "operator", choices: ["Addition", "subtraction", "multiplication", "division"],},
+]);
+ //conditional statement if else
+    if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+ } else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+ } else if(answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+ } else if(answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+} else{
+    console.log("please select valid operator")
+}
